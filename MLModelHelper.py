13c6418fabcb6919f5814c16dda1dd846fec6b2a
Piyashi Biswas <piyashi.biswas@gmail.com>
2025-11-25 23:44:45 -0800

    Added comments and renamed variables with snake-case convention

--- MLModelHelper.py
+++ MLModelHelper.py
@@ -3,8 +3,12 @@ import scipy.stats as stats
 from scipy.signal import welch, find_peaks
 
 
-def createFeatures(data, Fs):
-    featureNames = ['mean', 'median', 'std', 'variance', 'skew', 'kurtosis', 'Range', 'maxPeakWidth', 'maxTroughWidth', 'lfPower', 'hfPower', 'HjorthMobility', 'HjorthComplexity' ]
+def create_waveform_features(data, sampling_freq):
+    """Function creates waveform features given epoched data and sampling frequencies"""
+    feature_names = ['mean', 'median', 'std', 'variance', 'skew', \
+                     'kurtosis', 'amplitude_range', 'max_peak_width', \
+                        'max_trough_width', 'low_freq_power', 'high_freq_power', \
+                            'Hjorth_mobility', 'Hjorth_complexity' ]
 
     mean = np.mean(data, axis = 1)
     median = np.median(data, axis = 1)
@@ -13,56 +17,62 @@ def createFeatures(data, Fs):
     skew = stats.skew(data, axis = 1)
     kurtosis = stats.kurtosis(data, axis = 1)
 
-    Range = np.max(data, axis = 1) - np.min(data, axis = 1)
-    maxPeakWidth = findWidthZC(data, dir = 'peak')
-    maxTroughWidth = findWidthZC(data, dir = 'trough')
+    amplitude_range = np.max(data, axis = 1) - np.min(data, axis = 1)
+    max_peak_width = find_width_zerocrossing(data, direction = 'peak')
+    max_trough_width = find_width_zerocrossing(data, direction = 'trough')
 
-    freqs, psd = welch(data, fs=Fs, nperseg=np.size(data, axis = 1), axis = 1) #probably not the best idea with low frequencies
+    freqs, psd = welch(data, sampling_freq=sampling_freq, \
+                       nperseg=np.size(data, axis = 1), axis = 1) #unsuitable low frequencies
     freq_ind_low = np.where(freqs == 10)[0][0]
     freq_ind_high_1 = np.where(freqs == 10)[0][0]
     freq_ind_high_2 = np.where(freqs == 50)[0][0]
-    lfPower = np.sum(psd[:,0:freq_ind_low], axis = 1)
-    hfPower = np.sum(psd[:,freq_ind_high_1:freq_ind_high_2], axis = 1)
+    low_freq_power = np.sum(psd[:,0:freq_ind_low], axis = 1)
+    high_freq_power = np.sum(psd[:,freq_ind_high_1:freq_ind_high_2], axis = 1)
 
     # Hjorth
     diff_data = np.diff(data, axis = 1) #velocity
     diff2_data = np.diff(diff_data, axis = 1) #acceleration
-    activity = np.var(data, axis = 1)
-    mobility = np.sqrt(np.var(diff_data, axis = 1) / activity)
-    complexity = np.sqrt(np.var(diff2_data, axis = 1) / np.var(diff_data, axis = 1)) / mobility
+    hj_activity = np.var(data, axis = 1)
+    hj_mobility = np.sqrt(np.var(diff_data, axis = 1) / hj_activity)
+    hj_complexity = np.sqrt(np.var(diff2_data, axis = 1) / \
+                            np.var(diff_data, axis = 1)) / hj_mobility
 
-    features = np.array([mean, median, std, variance, skew, kurtosis, Range, maxPeakWidth, maxTroughWidth, lfPower, hfPower, mobility, complexity]).T
+    features = np.array([mean, median, std, variance, skew, \
+                         kurtosis, amplitude_range, max_peak_width, \
+                            max_trough_width, low_freq_power, high_freq_power, \
+                                hj_mobility, hj_complexity]).T
 
-    return [featureNames, features]
+    return [feature_names, features]
 
-def findWidthZC(data, dir = 'peak'):
-
-    allMaxWidths = np.zeros(np.size(data, axis = 0))
+def find_width_zerocrossing(data, direction = 'peak'):
+    """Function finds the width for highest peak or lowest trough \
+        from mean normalized epoched data"""
+    all_max_widths = np.zeros(np.size(data, axis = 0))
     for epoch in range(0, np.size(data, axis = 0)):
         data_1d = data[epoch,:]
 
-        meanCorrectedData = data_1d - np.mean(data_1d)
-        zcPts = np.where(np.diff(np.sign(meanCorrectedData)))[0] #zero crossing points
-        zcPts = np.append(0,zcPts)
-        zcPts = np.append(zcPts, np.size(meanCorrectedData))
+        mean_corrected_data = data_1d - np.mean(data_1d)
+        zc_pts = np.where(np.diff(np.sign(mean_corrected_data)))[0] #zero crossing points
+        zc_pts = np.append(0,zc_pts)
+        zc_pts = np.append(zc_pts, np.size(mean_corrected_data))
 
-        if dir == 'peak':
-            allPeaks, _ = find_peaks(meanCorrectedData,1)
-            maxPeakIndInd = np.argmax(meanCorrectedData[allPeaks])
-        elif dir == 'trough':
-            allPeaks, _ = find_peaks(-meanCorrectedData,1)
-            maxPeakIndInd = np.argmin(meanCorrectedData[allPeaks])
+        ind_max_pk_ind = 0
+        if direction == 'peak':
+            all_peaks, _ = find_peaks(mean_corrected_data,1)
+            ind_max_pk_ind = np.argmax(mean_corrected_data[all_peaks])
+        elif direction == 'trough':
+            all_peaks, _ = find_peaks(-mean_corrected_data,1)
+            ind_max_pk_ind = np.argmin(mean_corrected_data[all_peaks])
 
-        maxPeakInd = allPeaks[maxPeakIndInd]
+        max_pk_ind = all_peaks[ind_max_pk_ind]
         width_at_maxpk = 0
 
         # For max peak, find closest zero crossing points and calculate width
-        left_pt = zcPts[zcPts < maxPeakInd]
-        right_pt = zcPts[zcPts > maxPeakInd]
+        left_pt = zc_pts[zc_pts < max_pk_ind]
+        right_pt = zc_pts[zc_pts > max_pk_ind]
         if left_pt.size > 0 and right_pt.size > 0:
             width_at_maxpk = right_pt[0] - left_pt[-1]
 
-        allMaxWidths[epoch] = width_at_maxpk
+        all_max_widths[epoch] = width_at_maxpk
 
-    return allMaxWidths
-
+    return all_max_widths
--- signalProcessingHelper.py
+++ signalProcessingHelper.py
@@ -1,44 +1,41 @@
 from scipy.signal import butter, filtfilt
 import numpy as np
 
-def zeroPhaseFreqFilter(signal, cutoff, fs, filtType, order =4):
-    fNyquist = 0.5 * fs  # Nyquist frequency
+def zero_phase_freq_filter(signal, cutoff, fs, filt_type, order = 4):
+    """filters using zero phase butterworth filter based on parameters"""
+    nyquist_freq = 0.5 * fs  # Nyquist frequency
     if isinstance(cutoff, (list, tuple)):
-        normal_cutoff = [freq / fNyquist for freq in cutoff]
+        normal_cutoff = [freq / nyquist_freq for freq in cutoff]
     else:
-        normal_cutoff = cutoff / fNyquist
+        normal_cutoff = cutoff / nyquist_freq
 
-    b, a = butter(order, normal_cutoff, btype=filtType, analog=False)
+    b, a = butter(order, normal_cutoff, btype=filt_type, analog=False)
     filtered_signal = filtfilt(b, a, signal)
     return filtered_signal
 
-def epochSignal(signal, epoch_length, overlap):
-    # epoch_length = epoch_length.astype(np.int32)
-    # overlap = overlap.astype(np.int32)
-    
+def epoch_signal(signal, epoch_length, overlap = 0):
+    """divides signal into epochs with specified overlap"""
     if overlap != 0:
-        signal_append_end = np.concatenate([signal, np.zeros((signal.shape[0], overlap), dtype=signal.dtype)], axis=1)
-        signal_append = np.concatenate([np.zeros((signal.shape[0], overlap),dtype=signal.dtype),signal_append_end], axis=1)
-        
-        numWindows = (signal_append.shape[1] - epoch_length)// overlap + 1
-        epoched_signal = np.empty((signal_append.shape[0], numWindows, epoch_length))
+        signal_append_end = np.concatenate([signal, \
+                                            np.zeros((signal.shape[0], overlap), \
+                                                     dtype=signal.dtype)], axis=1)
+        signal_append = np.concatenate([np.zeros((signal.shape[0], overlap),\
+                                                 dtype=signal.dtype),signal_append_end], axis=1)
+        num_windows = (signal_append.shape[1] - epoch_length)// overlap + 1
+        epoched_signal = np.empty((signal_append.shape[0], num_windows, epoch_length))
         for i in range(0,signal_append.shape[0]):
             ind = 0
-            for j in range(0,numWindows):
+            for j in range(0,num_windows):
                 epoched_signal[i,j,:] = signal_append[i,ind:ind+epoch_length]
                 ind = ind+overlap
 
     else:
-        numWindows = (signal.shape[1]//epoch_length)
-
-        epoched_signal = np.empty((signal.shape[0], numWindows, epoch_length))
+        num_windows = (signal.shape[1]//epoch_length)
+        epoched_signal = np.empty((signal.shape[0], num_windows, epoch_length))
         for i in range(0,signal.shape[0]):
             ind = 0
-            for j in range(0,numWindows):
+            for j in range(0,num_windows):
                 epoched_signal[i,j,:] = signal[i,ind:ind+epoch_length]
                 ind = ind+epoch_length
-        
-    
+
     return epoched_signal
-
-
